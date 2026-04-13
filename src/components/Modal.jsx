2b089d4cd9c6282ab68b5de94f4cb2497b2361ee
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2026-04-13 18:21:24 +0530

    Use dangerouslySetInnerHTML in modal body
    Updated modal body to use dangerouslySetInnerHTML for rendering children.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -73,7 +73,10 @@ const Modal = ({ open, title, children, onClose }) => {
               ✕
             </motion.button>
           </div>
-          <div className="modal-body">{children}</div>
+          <div
+            className="modal-body"
+            dangerouslySetInnerHTML={{ __html: children }}>
+          </div>
         </motion.div>
       </motion.div>
     </AnimatePresence>
